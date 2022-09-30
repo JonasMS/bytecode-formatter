a0668314bd08625eab95924e869afa1e63296890
Jonas Sota <jonas@loopcrypto.xyz>
2022-09-29 22:16:24 -0600

    impl --function-call flag

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ use clap::Parser;
 #[clap(author, version, about, long_about = None)]
 struct Args {
     bytecode: String,
+    
+    #[clap(long, short = 'f')]
+    function_call: bool,
 }
 
 fn main() {
     let args = Args::parse();
+
+    let mut idx = 2;
+    let mut line_count = 0;
     
-    /* Print Fxn Selector */
-    println!("0: {}", &args.bytecode[0..9]);
+    if args.function_call {
+        /* Print Fxn Selector */
+        println!("{}: {}", line_count, &args.bytecode[0..9]);
+        idx = 10;
+        line_count += 1;
+    }
 
     /* Print Call Data */
-    let mut idx = 10;
-    let mut line_count = 1;
     while idx + 64 <= args.bytecode.len() {
         println!("{}: {}", line_count, &args.bytecode[idx..idx+64]);
         idx += 64;
